8f71c5f7d6917236f17ab75d497b514c61d23ef2
Love Gehlin <love.gehlin@gmail.com>
2013-10-10 19:41:32 +0200

    added backbone template, view and model for loginscreen

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -32,7 +32,50 @@ require.config({
 });
 
 require([
-    'backbone'
+  'backbone'
 ], function (Backbone) {
-    Backbone.history.start();
+  // Vettefan vad detta är?
+  Backbone.history.start();
+
+  // Modellen som håller login-data.
+  var Credentials = Backbone.Model.extend({
+    defaults: {
+      user: '',
+      password: ''
+    }
+  });
+
+  // Vyn som hanterar logindelen.
+  var LoginView = Backbone.View.extend({
+    // Wrappern som templaten skall hamna i. Skall finnas definierad i HTML:en
+    el: $('.login-container'),
+    // Templaten som skall renderas. Ligger definierat som ett script längst ner på sidan
+    loginTemplate: _.template($('#login-template').html()),
+
+    events: {
+      "click #loginbutton": "login"
+    },
+
+    initialize: function(){
+      this.render();
+    },
+
+    render: function () {
+        // Insertar templaten till den valda containern som är definierad med el.
+        // this.model.toJSON() är modellen man skickar in till vyn, se slutet av filen.
+        // Templaten får sedan ut user och password enligt självförklarande syntax i index.html
+        this.$el.html(this.loginTemplate(this.model.toJSON()));
+    },
+
+    login: function() {
+      var user = this.model.get('user');
+      var password = this.model.get('password');
+      // Detta funkar inte riktigt. Den uppdaterar inte modellen när man skriver något. Ändrar man defaults funkar det dock.
+      alert("Loggat in med " + user + " och password " + password);
+      return false;
+    }
+  });
+
+  window.LoginView = new LoginView({model: new Credentials()});
 });
+
