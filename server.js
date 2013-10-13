6d31a82c81edeb41fa18a4f17acdc9ca036c8e36
Soroush Hakami <soroush.hakami@gmail.com>
2013-10-13 14:25:00 +0200

    build and autopush

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 'use strict';
 var express = require('express'),
 	app = express(),
-	env = app.get('env');
+	env = app.get('env'),
+	gzippo = require('gzippo');
 
 
 if ('staging' === env) {
 	app.use(express.logger('dev'));
 	app.use(express.errorHandler());
-	app.use(express.static(__dirname + '/dist'));
+	app.use(gzippo.staticGzip(__dirname + '/dist'));
 }
 
 if ('production' === env) {
-	app.use(express.static(__dirname + '/dist'));
+	app.use(gzippo.staticGzip(__dirname + '/dist'));
 }
 
 app.set('port', process.env.PORT || 3000);
